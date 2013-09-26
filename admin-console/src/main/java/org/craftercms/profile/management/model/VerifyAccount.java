842ac931e6e821b24eb2d8da7cd1de76ee5e7f03
alvarog <agonzalez@rivetlogic.com>
2013-09-26 14:33:42 -0600

    CRAFTERCMS-295: Account verification model

---
+++ admin-console/src/main/java/org/craftercms/profile/management/model/VerifyAccount.java
@@ -0,0 +1,15 @@
+package org.craftercms.profile.management.model;
+
+public class VerifyAccount {
+	
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
