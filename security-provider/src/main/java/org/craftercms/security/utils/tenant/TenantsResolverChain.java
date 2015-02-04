fa45540dbb71a29d65d024d6e1d589fad2e00d4a
Alfonso Vásquez <avasquez@rivetlogic.com>
2015-02-04 16:45:44 -0600

    Added a chainAllTenants flag to TenantsResolverChain.

--- security-provider/src/main/java/org/craftercms/security/utils/tenant/TenantsResolverChain.java
+++ security-provider/src/main/java/org/craftercms/security/utils/tenant/TenantsResolverChain.java
@@ -1,5 +1,7 @@
 package org.craftercms.security.utils.tenant;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang3.ArrayUtils;
@@ -7,30 +9,45 @@ import org.craftercms.profile.social.utils.TenantsResolver;
 import org.springframework.beans.factory.annotation.Required;
 
 /**
- * {@link org.craftercms.profile.social.utils.TenantsResolver} that uses a chain of other resolvers to resolve the
- * tenants.
+ * {@link org.craftercms.profile.social.utils.TenantsResolver} that returns the first tenants resolved by any resolver.
+ * If the flag {@code chainAllTenants} is set, instead all non empty tenants returned by the resolvers are returned.
  *
  * @author avasquez
  */
 public class TenantsResolverChain implements TenantsResolver {
 
     protected List<TenantsResolver> resolvers;
+    protected boolean chainAllTenants;
 
     @Required
     public void setResolvers(final List<TenantsResolver> resolvers) {
         this.resolvers = resolvers;
     }
 
+    public void setChainAllTenants(final boolean chainAllTenants) {
+        this.chainAllTenants = chainAllTenants;
+    }
+
     @Override
     public String[] getTenants() {
+        List<String> finalTenants = null;
+
         for (TenantsResolver resolver : resolvers) {
             String[] tenants = resolver.getTenants();
             if (ArrayUtils.isNotEmpty(tenants)) {
-                return tenants;
+                if (chainAllTenants) {
+                    if (finalTenants == null) {
+                        finalTenants = new ArrayList<>();
+                    }
+
+                    finalTenants.addAll(Arrays.asList(tenants));
+                } else {
+                    return tenants;
+                }
             }
         }
 
-        return null;
+        return finalTenants != null ? finalTenants.toArray(new String[finalTenants.size()]) : null;
     }
 
 }
