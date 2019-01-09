5c7ddf1df84cca23151cb161d2102fc69bb9da3a
Dejan Brkic <dejan.brkic@craftersoftware.com>
2019-01-09 22:03:20 +0100

    Copyright update

--- admin-console/src/main/webapp/resources/js/bootstrap-growl.js
+++ admin-console/src/main/webapp/resources/js/bootstrap-growl.js
@@ -1,6 +1,19 @@
-/*! Bootstrap Growl - v1.0.6 - 2014-01-29
-* https://github.com/mouse0270/bootstrap-growl
-* Copyright (c) 2014 Remable Designs; Licensed MIT */
+/*
+ * Copyright (C) 2007-2019 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 ;(function($, window, document, undefined) {
 	"use strict";
 	var bootstrap_growl_remove = [];
