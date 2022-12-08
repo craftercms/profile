eb76d4f3605e9d2c53c6421ff1522bbda6b3e4e2
Jonathan Méndez <jonnathan.mendez@gmail.com>
2022-12-08 08:54:14 -0600

    Update method to new access modifier (#220)

--- server/src/main/java/org/craftercms/profile/controllers/rest/MonitoringController.java
+++ server/src/main/java/org/craftercms/profile/controllers/rest/MonitoringController.java
@@ -40,7 +40,7 @@ public class MonitoringController extends MonitoringRestControllerBase {
     }
 
     @Override
-    protected String getConfiguredToken() {
+    public String getConfiguredToken() {
         return authorizationToken;
     }
 
