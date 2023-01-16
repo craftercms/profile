1eb7b68452e17c4ce695506058fb23ec3e282712
Jonathan Méndez <jonnathan.mendez@gmail.com>
2023-01-16 09:38:06 -0600

    Validations refactor (#223)
    * Validations refactor
    
    * Copyright notice.

--- server/src/main/java/org/craftercms/profile/controllers/rest/MonitoringController.java
+++ server/src/main/java/org/craftercms/profile/controllers/rest/MonitoringController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ * Copyright (C) 2007-2023 Crafter Software Corporation. All Rights Reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as published by
@@ -32,16 +32,9 @@ public class MonitoringController extends MonitoringRestControllerBase {
 
     public final static String URL_ROOT = "/api/1";
 
-    protected String authorizationToken;
-
     @ConstructorProperties({"authorizationToken"})
     public MonitoringController(final String authorizationToken) {
-        this.authorizationToken = authorizationToken;
-    }
-
-    @Override
-    public String getConfiguredToken() {
-        return authorizationToken;
+        super(authorizationToken);
     }
 
 }
